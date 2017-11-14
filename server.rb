479487667ff2a6827f0427dc4415231425a1d49c
kyjoya <kyleejacker@gmail.com>
2017-11-14 08:55:18 -0500

    Initial app

---
+++ server.rb
@@ -0,0 +1,15 @@
+require "sinatra"
+require "json"
+require 'pry'
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+set :public_folder, File.join(File.dirname(__FILE__), "public")
+
+get "/" do
+  erb :home
+end
+
+get "/api/v1/questions.json" do
+  #For Exceeds Only
+  #Use questions.json to pull a random question for your question data
+end
